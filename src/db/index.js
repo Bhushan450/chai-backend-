c6c32e70331186278fce5f110f48a0faab7437df
bhushan450 <bhushanmhaske45@gmail.com>
2025-09-23 19:06:35 +0530

    updated repo

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_Name } from "../constants.js";
 const connectDB= async ()=>{
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
-        console.log(`\n mongoDB connected !! DB Host${connectionInstance.connection.host}`);
+        console.log(`\n mongoDB connected !! DB Host:${connectionInstance.connection.host}`);
         
         
     } catch (error) {
@@ -12,4 +12,5 @@ const connectDB= async ()=>{
         process.exit(1)     
     }
 }
+
 export default connectDB
