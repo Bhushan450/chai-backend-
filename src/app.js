c79d717065e9b614ddd8726aac707278c303cf38
bhushan450 <bhushanmhaske45@gmail.com>
2025-10-07 20:09:30 +0530

    routes,controllers writing

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(express.json({limit:"20kb"}))  // when data comes from 'FORM'
 app.use(express.urlencoded({extended: true,limit:"20kb"})) // when data comes from 'URL'
 app.use(express.static("public"))  // for storing photos etc. into our server 
 app.use(cookieParser())
+
+//routes import 
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration 
+app.use("/api/v1/users",userRouter)
+
+// https://localhost:8000/api/v1/users/register
+
 export default app
 
-//
